1254357eaa093e2be94de31c20392419ab586de3
Tiago Antao <tiago@tiago.org>
2023-11-22 18:00:34 -0500

    fs module start

---
+++ include/fs.h
@@ -0,0 +1,5 @@
+#include <stdbool.h>
+
+
+bool file_exists(const char* path);
+int get_file_size(const char* path);
---
+++ src/fs.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <sys/stat.h>
+
+
+bool file_exists(const char *path) {
+    struct stat st;
+    return stat(path, &st) == 0;
+}
+
+
+int get_file_size(const char *path) {
+    struct stat st;
+    if (stat(path, &st) == 0)
+        return st.st_size;
+    return -1;
+}
