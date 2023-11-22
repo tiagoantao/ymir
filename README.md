6c8e5edb53a5c7d33afd518729ddda7a47eb6fb8
Tiago Antao <tiago@tiago.org>
2023-11-22 18:00:56 -0500

    more png testing

--- README.md
+++ README.md
@@ -2,17 +2,38 @@
 
 C codebase library.
 
+This is a bunch of functions that I personally use a lot when coding in C.
+You probably don't want to use it wholesale, but maybe take part of the code
+yourself. If you use part of the code, please attribute it in whatever way
+is more convenient.
 
 
+Tested on Linux only.
+
+
+
+```
 meson setup build
+
+cd build
 
 meson compile
 
 meson test
-
+```
 
 # Requirements
 
-meson ninja-build check
+Specified as Debian-based dependencies. Similar packages should exist
+for other Linuxes (and *nix derivatives).
 
-glib-2 libpng
+```
+apt-get install meson ninja-build check
+
+apt-get install glib-2 libpng
+```
+
+# TODO
+
+- test coverage
+- memory checks on build (valgrind or something)
--- tests/test_graphics.c
+++ tests/test_graphics.c
@@ -3,26 +3,50 @@
 
 #include <check.h>
 
+#include "fs.h"
 #include "graphics.h"
 
 START_TEST(test_write_png) {
-   int width = 100;
+    int width = 100;
     int height = 100;
 
-    // Allocate memory for the test image
     pixel_t **image = malloc(height * sizeof(pixel_t *));
     for (int y = 0; y < height; y++) {
         image[y] = malloc(width * sizeof(pixel_t));
         for (int x = 0; x < width; x++) {
-            // Fill the image with a simple pattern, e.g., red gradient
+            // Fill the image with a red gradient
             image[y][x].r = (uint8_t)((255 * y) / height);
             image[y][x].g = 0;
             image[y][x].b = 0;
         }
     }
 
-    // Call the function to write the image to a PNG file
     write_png("test_image.png", image, height, width);
+
+    for (int y = 0; y < height; y++) {
+        free(image[y]);
+    }
+    free(image);
+
+    ck_assert(file_exists("test_image.png"));
+    ck_assert(get_file_size("test_image.png") > 0);
+}
+END_TEST
+
+
+START_TEST(test_read_png){
+    pixel_t **image = NULL;
+    int height = 0;
+    int width = 0;
+
+    read_png("../tests/rt.png", &image, &height, &width);
+
+    // Assertions to check if the function succeeded
+    ck_assert(image != NULL);
+    ck_assert(height == 1000);
+    ck_assert(width == 2000);
+
+    // Add more checks here if necessary, for example, inspecting some pixel values
 
     // Free the allocated memory
     for (int y = 0; y < height; y++) {
@@ -39,6 +63,7 @@ Suite *graphics_suite(void) {
     s = suite_create("Graphics");
     tc_png = tcase_create("PNG");
     tcase_add_test(tc_png, test_write_png);
+    tcase_add_test(tc_png, test_read_png);
     suite_add_tcase(s, tc_png);
     return s;
 }
